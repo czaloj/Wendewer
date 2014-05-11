be2a0cdff652df8e9df8e9d68be190b7937ec1cd
Cristian Zaloj <czaloj@gmail.com>
2014-05-10 21:38:54 -0400

    Initial

---
+++ Wendewer/Wendewer/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using BlisterUI;
+using BlisterUI.Input;
+
+namespace Wdw {
+    public class App : MainGame {
+
+        protected override void BuildScreenList() {
+            screenList = new ScreenList(this, 0,
+                new FalseFirstScreen(1)
+                );
+        }
+        protected override void FullInitialize() {
+            WMHookInput.Initialize(Window);
+        }
+        protected override void FullLoad() {
+        }
+
+        static void Main(string[] args) {
+            using(App app = new App()) {
+                app.Run();
+            }
+        }
+    }
+}
